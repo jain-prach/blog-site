8a8cadff50dd0c0740912224cdb2b75215d4bd4e
jain-prach <prachijain8917@gmail.com>
2023-01-13 11:56:00 +0530

    initial commit

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Blog Site</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Blog Site to learn Next.js" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+
+const Page: NextPage = () => {
+  return (
+    <div>: NextPage</div>
+  )
+}
+
+export default Page;
