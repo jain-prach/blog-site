dcf13b75c5061be61da8813c58bf088aad6baf2b
jain-prach <prachijain8917@gmail.com>
2023-01-14 13:29:04 +0530

    Props and {params.slug} - [slugs]/page.tsx

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,11 +1,26 @@
 import { NextPage } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+import { getBlogs } from "../../../lib/blogs";
 
-const BlogDetail: NextPage = () => {
+interface Params extends ParsedUrlQuery {
+    slug: string;
+}
+type Props = {
+    params: Params;
+}
+const BlogDetail: NextPage<Props> = ({params}) => {
     return (
         <div>
-            Generic Page!
+            Generic Page - {params.slug}
         </div>
     )
 }
 
+export function generateStaticParams() {
+    const blogs = getBlogs();
+
+    return blogs.map(blog => ({
+        slug: blog.slug
+    }))
+}
 export default BlogDetail;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,11 @@ export default function RootLayout({
       
       <head />
       <body>
-        {children}
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          {children}
+        </div>
+      </div>
       </body>
     </html>
   )
