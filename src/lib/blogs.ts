e38f71a2c2721c668b50e5d84c94ef05653eae00
jain-prach <prachijain8917@gmail.com>
2023-01-14 10:19:45 +0530

    getting blogs through file names=blogs.ts

---
+++ src/lib/blogs.ts
@@ -0,0 +1,17 @@
+import path from "path";
+import { join } from "path";
+import fs from "fs"; /* file system */
+
+const getDir = (path: string) => join(process.cwd(), path); /* get Directory process.cwd = returns the name of the current working directory */
+
+const getFileNames = (dir: string): string[] => { 
+    return fs.readdirSync(dir);
+}/* helper function */
+
+const getBlogFileNames = () => {
+    return getFileNames(getDir("/content/blogs")); /* src/ */
+}
+
+export {
+    getBlogFileNames,
+}
